fae779037343e30eec1c713ca1e0fd421d4039ca
EmmaaDy <emma.dybdorf@hotmail.com>
2024-06-14 11:04:26 +0200

    some fix with menu
    Readme, menu.js, menu.Schema.js

--- src/models/menuSchema.js
+++ src/models/menuSchema.js
@@ -1,19 +1,10 @@
-//  Schema for the Menu.  
-
 import Joi from 'joi';
 
 const menuItemSchema = Joi.object({
+  id: Joi.string().optional(), // Gör id valfritt
   title: Joi.string().min(3).max(100).required(),
   desc: Joi.string().min(10).max(500).required(),
-  price: Joi.number().positive().required(),
-  createdAt: Joi.date().optional(),
-  modifiedAt: Joi.date().optional()
+  price: Joi.number().positive().required()
 });
 
-const updateMenuItemSchema = Joi.object({
-  desc: Joi.string().min(10).max(500).optional(),
-  price: Joi.number().positive().optional(),
-  modifiedAt: Joi.date().optional()
-});
-
-export { menuItemSchema ,updateMenuItemSchema};
+export { menuItemSchema };
